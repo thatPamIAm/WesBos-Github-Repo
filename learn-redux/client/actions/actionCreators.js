f7e771f43e767471d6ff24d50e7bd1998582249d
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-03-09 20:38:48 -0700

    Create action creators file

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -1,12 +1,12 @@
-//increment
-export function increment(index){
+// increment
+export function increment(index) {
   return {
     type: 'INCREMENT_LIKES',
     index
   }
 }
 
-//add comment
+// add comment
 export function addComment(postId, author, comment) {
   return {
     type: 'ADD_COMMENT',
@@ -15,12 +15,13 @@ export function addComment(postId, author, comment) {
     comment
   }
 }
-//remove comment
+
+// remove comment
 
 export function removeComment(postId, i) {
   return {
     type: 'REMOVE_COMMENT',
     i,
-    postID
+    postId
   }
 }
